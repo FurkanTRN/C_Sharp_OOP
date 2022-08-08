1a0c96608b1f233d4b8303d8c32a5864dbbe4d78
FurkanTRN <contact.furkantrn@gmail.com>
2022-08-09 01:12:31 +0300

    added Project

---
+++ RecapDemo2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace RecapDemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Logger = new SmsLogger();
+            customerManager.Add();
+
+            Console.ReadLine();
+        }
+    }
+
+    class CustomerManager
+    {
+        public ILogger Logger { get; set; }
+        public void Add()
+        {
+           Logger.Log();
+            Console.WriteLine("Customer added!");
+        }
+    }
+
+    class DatabaseLogger:ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("logged to database!");
+        }
+    }
+
+    class FileLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("logged to file!");
+        }
+    }
+
+    class SmsLogger : ILogger
+    {
+        public void Log()
+        {
+            Console.WriteLine("logged to sms!");
+        }
+    }
+
+
+
+    interface ILogger
+    {
+        void Log();
+    }
+}
